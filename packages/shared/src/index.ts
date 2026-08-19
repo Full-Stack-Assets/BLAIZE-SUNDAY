6333f48d66f385316bbfe4eec390ab357eda3f7b
Nic Albertson <nicholas.albertson@students.maestrocollege.edu>
2026-08-19 06:49:12 -0400

    feat: export G2 voice evaluator

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,4 @@ export * from "./creative-field.ts";
 export * from "./domain.ts";
 export * from "./project-state-machine.ts";
 export * from "./voice-registry.ts";
+export * from "./voice-g2-evaluation.ts";
